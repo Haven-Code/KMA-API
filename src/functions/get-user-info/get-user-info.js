e73fb5d1b4ff9079e0620c2026f248a6130a4903
Phuc Dang <phuc.px.371@gmail.com>
2020-08-25 12:20:58 +0700

    update get-user-info

---
+++ src/functions/get-user-info/get-user-info.js
@@ -0,0 +1,45 @@
+// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
+const CMC = require("cmcsoft-iu")({ HOST_API: "http://qldt.actvn.edu.vn" })
+
+const headers = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Headers': 'Content-Type',
+	'Access-Control-Allow-Methods': 'GET, POST'
+};
+
+exports.handler = async (event, context) => {
+	if (event.httpMethod != "POST") return { statusCode: 200, body: JSON.stringify({ code: "ERROR", message: "Invalid Method" }) }
+
+	try {
+		//const rqBody = event.queryStringParameters
+		const rqBody = JSON.parse(event.body)
+
+		try {
+
+			const api = await CMC({ user: rqBody.username, pass: rqBody.password })
+			const studentInfo = await api.studentProfile.show()
+			const schedule = await api.studentTimeTable.showTimeTable()
+
+			let data = {
+				studentInfo: studentInfo,
+				schedule: schedule
+			}
+
+			return {
+				statusCode: 200,
+				headers,
+				body: JSON.stringify({ code: "SUCCESS", data: data })
+			}
+
+		} catch (err) {
+			return {
+				statusCode: 200,
+				headers,
+				body: JSON.stringify({ code: "ERROR", message: err })
+			}
+		}
+
+	} catch (err) {
+		return { statusCode: 500, headers, body: err.toString() }
+	}
+}
